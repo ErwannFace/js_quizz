da2cdcfb0413461a6fd47d341307840cd7c7d19d
Megaju <julien_malle@outlook.fr>
2016-07-24 16:10:40 +0200

    Ajout d'une page pour tester la mémoire

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $(".ans-good").on({
-    /*mouseenter: function(){
-        $(this).css("background-color", "lightgray");
-    }, 
-    mouseleave: function(){
-        $(this).css("background-color", "lightblue");
-    }, */
     click: function(){
         $("td").removeClass("danger");
         $(".ans-good").addClass("success");
