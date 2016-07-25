d22e87d59e8b32591f4f067ff4e6da0cb3b5ac1a
Mopi <solene.huault@gmail.com>
2016-07-25 19:43:32 +0200

    une réponse par question, répondre avant de passer à la question suivante

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 var i = 1;
 var j = 2;
-
-$("#next").click(function() {
-    $("#" + i).hide();
-    $("#" + j).show();
-    i++;
-    j++;
+var answer = false;
+$("#next").click(function () {
+    if (j < 11 && answer === true) {
+        $("#" + i).hide();
+        $("#" + j).show();
+        i++;
+        j++;
+        answer = false;
+    }
 });
-
-$("#previous").click(function() {
-    i--;
-    j--;
-    $("#" + i).show();
-    $("#" + j).hide();
+$("#previous").click(function () {
+    if (i > 1) {
+        i--;
+        j--;
+        $("#" + i).show();
+        $("#" + j).hide();
+    }
 });
 
 function answer_right(target) {
-    target.style.background="green";
+    if (answer === false) {
+        target.style.background = "green";
+        answer = true;
+    }
 }
 
 function answer_false(target) {
-    target.style.background="red";
+    if (answer === false) {
+        target.style.background = "red";
+        answer = true;
+    }
 };
