389f985a44971b287fa5e407bd9bf956a372efcd
Annie-Line <leclair.an@gmail.com>
2016-07-25 22:36:51 +0200

    Probleme changement question réglé

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 /*Link tutoriel : https://www.youtube.com/watch?v=d_UuOVhuCF8*/
 /*Initialisation des différentes variables*/
 
-var pos = 0, test, test_status, reponse, reponses, r1, r2, r3, r4, correct=0;
-var question = 0;
+var pos = 0, test, test_status, question, reponse, reponses, r1, r2, r3, r4, correct=0;
 
 /*Création des questions*/
 
 var questions = [
 
-	["Dans quelle série joue Kerry Washington ?", "The Blacklist", "Scandal", "Triple 9", "Arrow", "1"],
-	["Qui incarne le rôle de Franck Underwood dans House of Cards ?", "Patrick Dempsey", "Kevin Hart", "Al Pacino", "Kevin Spacey", "3"],
-	["Qui a remplacé DSK à la présidence du FMI ?", "Michel Alliot-Marie", "Michel Sapin","Chrisitine Lagarde","Christine Taubira", "2" ],
-	["Où se trouve Wallis-et-Futuna ?", "Pacifique Sud", "Antilles", "Mer des Caraïbes", "Océan Indien", "0"],
-	["Quel est le fondateur de Snapchat ?", "Xavier Niel", "Evan Spieggle", "Adolf Dassler", "Eric Cantona", "Brarack Obama","1"],
+	["Dans quelle série joue Kerry Washington ?", "The Blacklist", "Scandal", "Triple 9", "Arrow", "2"],
+	["Qui incarne le rôle de Franck Underwood dans House of Cards ?", "Patrick Dempsey", "Kevin Hart", "Al Pacino", "Kevin Spacey", "4"],
+	["Qui a remplacé DSK à la présidence du FMI ?", "Michel Alliot-Marie", "Michel Sapin","Chrisitine Lagarde","Christine Taubira", "3" ],
+	["Où se trouve Wallis-et-Futuna ?", "Pacifique Sud", "Antilles", "Mer des Caraïbes", "Océan Indien", "1"],
+	["Quel est le fondateur de Snapchat ?", "Xavier Niel", "Evan Spieggle", "Adolf Dassler", "Eric Cantona", "Brarack Obama","2"],
 	["Quel est la capital du Brésil ?", "Sao Paulo", "Brasilia", "Rio de Janeiro", "Sao Luis","1"],
-	["Qui est le président de la Fondation Face ?", "Sébastien Ramos", "Mark Zuckerberg", "Gérard MESTRALLET", "Alain ROUMILHAC", "2"],
-	["Quel est le pays le plus pauvres ?", "Mongolie", "Kirghizstan", "Jamaïque", "Bénin", "1"],
-	["Dans quel état des Etats-Unis se trouve Chicago ?", "Mineapolis", "Missouri", "Pennsylvanie", "Illinois", "3"],
-	["Où est né Brarack Obama ?", "Kinshasa", "Honolulu", "Naïrobi", "Los Angeles", "1"],
+	["Qui est le président de la Fondation Face ?", "Sébastien Ramos", "Mark Zuckerberg", "Gérard MESTRALLET", "Alain ROUMILHAC", "3"],
+	["Quel est le pays le plus pauvres ?", "Mongolie", "Kirghizstan", "Jamaïque", "Bénin", "2"],
+	["Dans quel état des Etats-Unis se trouve Chicago ?", "Mineapolis", "Missouri", "Pennsylvanie", "Illinois", "4"],
+	["Où est né Brarack Obama ?", "Kinshasa", "Honolulu", "Naïrobi", "Los Angeles", "2"],
 
 ];
 /*Simplification du document.getElementById(x) */
@@ -29,8 +28,12 @@ function RendreQuestion(){
 	console.log(RendreQuestion);
 	test = _("test");
 
-	if ([pos]>= questions.length){
+	if (pos>= questions.length){
 		test.innerHTML = "<h2>Vous avez "+correct+" of "+questions.length+" questions correct</h2>";
+		_("test_status").innerHTML = "Quizz terminé !!";
+		pos = 0;
+		correct = 0;
+		return false;
 	}
 
 	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
@@ -51,7 +54,7 @@ function checkAnswer(){
 		console.log(checkAnswer);
 	reponses = _("reponses");
 	for (var i=0; i<reponses.length; i++){
-		if (reponses[i].checked){
+		if (reponses[i].onclick){
 			reponse = reponses[i].value;
 		}
 	}
@@ -65,7 +68,7 @@ function ReturnQ(){
 	console.log(ReturnQ);
 	reponses = document.getElementById("reponses");
 	for (var i=0; i>reponses.length; i++){
-		if (reponses[i].checked){
+		if (reponses[i].selected){
 			reponse = reponses[i].value;
 		}
 	}
