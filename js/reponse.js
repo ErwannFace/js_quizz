7cd72abb32f792e9af70383fdfed84c5b3e66f7a
Lise Poirier <lise.p.poirier@gmail.com>
2016-07-25 11:05:44 +0200

    Ajout du score

---
+++ js/reponse.js
@@ -0,0 +1,302 @@
+	score = 0;
+
+	//------------------------------------------------------
+	//Question 1
+	//Pour les mauvaises réponse.
+	function faux() {
+	    var erreur = document.getElementsByClassName("faux");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai() {
+	    var reponse = document.getElementsByClassName("vrai");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 2
+	//Pour les mauvaises réponse.
+	function faux1() {
+	    var erreur = document.getElementsByClassName("faux1");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai1");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai1() {
+	    var reponse = document.getElementsByClassName("vrai1");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 3
+	//Pour les mauvaises réponse.
+	function faux2() {
+	    var erreur = document.getElementsByClassName("faux2");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai2");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai2() {
+	    var reponse = document.getElementsByClassName("vrai2");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 4
+	//Pour les mauvaises réponse.
+	function faux3() {
+	    var erreur = document.getElementsByClassName("faux3");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai3");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai3() {
+	    var reponse = document.getElementsByClassName("vrai3");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 5
+	//Pour les mauvaises réponse.
+	function faux4() {
+	    var erreur = document.getElementsByClassName("faux4");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai4");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai4() {
+	    var reponse = document.getElementsByClassName("vrai4");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 6
+	//Pour les mauvaises réponse.
+	function faux5() {
+	    var erreur = document.getElementsByClassName("faux5");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai5");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai5() {
+	    var reponse = document.getElementsByClassName("vrai5");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 7
+	//Pour les mauvaises réponse.
+	function faux6() {
+	    var erreur = document.getElementsByClassName("faux6");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai6");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai6() {
+	    var reponse = document.getElementsByClassName("vrai6");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+	//------------------------------------------------------
+	//Question 8
+	//Pour les mauvaises réponse.
+	function faux7() {
+	    var erreur = document.getElementsByClassName("faux7");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai7");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai7() {
+	    var reponse = document.getElementsByClassName("vrai7");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+
+	//Question 9
+	//Pour les mauvaises réponse.
+	function faux8() {
+	    var erreur = document.getElementsByClassName("faux8");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai8");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai8() {
+	    var reponse = document.getElementsByClassName("vrai8");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	}
+
+
+
+
+	//Question 10
+	//Pour les mauvaises réponse.
+	function faux9() {
+	    var erreur = document.getElementsByClassName("faux9");
+	    var i;
+	    for (i = 0; i < erreur.length; i++) {
+	        //changement de couleur en rouge quand la réponse est fausse.
+	        erreur[i].style.backgroundColor = "rgba(255,184,184,0.5)";
+	        //devoile la bonne réponse en vert.
+	        var reponse = document.getElementsByClassName("vrai9");
+	        reponse[0].style.backgroundColor = "rgba(51,255,51,0.4)";
+	        //permet une transition plus douce.
+	        erreur[i].style.transitionDuration = "2s";
+	        reponse[0].style.transitionDuration = "2s";
+	        alert(score + " / 10")
+	    }
+	}
+
+	//Pour la bonne réponse.
+	function vrai9() {
+	    var reponse = document.getElementsByClassName("vrai9");
+	    //changement de couleur en vert quand la réponse est vrai.
+	    reponse[0].style.backgroundColor = "rgba(51,255,51,0.3)";
+	    //permet une transition plus douce.
+	    reponse[0].style.transitionDuration = "2s";
+	    score++;
+	    alert(score + " / 10");
+	}
