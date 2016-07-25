4b44c4bf05aa04d4667e2930214e94a3714cab19
jipee37 <jipee37@yahoo.fr>
2016-07-26 01:42:00 +0200

    second commit: first question made

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ $(function() {
 
 //stylisation en jquery
 var q = $('.question');          
-q.css('background', '#9EEAE0');  
-q.css('border-style', 'groove'); 
-q.css('border-width', '4px');    
+q.css('background', 'white');  
+q.css('border-style', 'solid'); 
+q.css('border-width', '4px');
+q.css('border-color', 'darkgray');    
 q.css('width', '900px');         
 q.css('height', '250px');        
 q.css('margin', '20px');         
@@ -18,5 +19,21 @@ $('.texte').css('width', '90%');
 $('img').css('float', 'right');
 $('img').css('margin-top', '80px');
 
-$("h2").css("color", "gray");
+$("h2").css("color", "darkgray");
 $("p").css("font-size", "17px");
+$("p").css("color", "gray");
+
+//  Action link «check responses»
+$('a').hover(
+          function() { 
+            $('.reponse').show();
+            if ($(':radio[id="r3"]:checked').val()) {
+              $('#img1').attr('src', 'right.png'); 
+              $('#reponse1').css('color', 'green');
+            }  
+            else {
+              $('#img1').attr('src', 'wrong.png');
+              $('#reponse1').css('color', 'red');
+            }
+          }
+        );       
