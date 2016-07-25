2093a6ac43de85cba80f7e9d6235b439c7e34bb6
jipee37 <jipee37@yahoo.fr>
2016-07-26 00:22:33 +0200

    first commit: stylisation jquery

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+      // obturation des réponses
+      $('.reponse').hide();
+      }); 
+
+//stylisation en jquery
+var q = $('.question');          
+q.css('background', '#9EEAE0');  
+q.css('border-style', 'groove'); 
+q.css('border-width', '4px');    
+q.css('width', '900px');         
+q.css('height', '250px');        
+q.css('margin', '20px');         
+
+
+$('.texte').css('float', 'left');
+$('.texte').css('width', '90%');
+$('img').css('float', 'right');
+$('img').css('margin-top', '80px');
+
+$("h2").css("color", "gray");
+$("p").css("font-size", "17px");
