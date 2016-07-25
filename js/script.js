5231c2137e17dc108392038b1c6fef9c1d02b1a4
Redfish <lesonduko@gmail.com>
2016-07-25 13:40:29 +0200

    initialisation du challenge quizz js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var question=[];
+var q1= "etes vous essez fort pour continuer ?";
+var rep=[];
+var rep1="oui";
+var rep2="non";
+var rep3="pietre";
+var rep4="je ne sais pas";
+
+
+question.push([q1, 1]);
+
+
+
+function AfficheRep(){
+$("#rep1").text(rep1);
+$("#rep2").text(rep2);
+$("#rep3").text(rep3);
+$("#rep4").text(rep4);
+}
+
