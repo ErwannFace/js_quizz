f98ee8769dafcdb5ab619686b08401d8549a7e63
tyjow <platon.jonathan@gmail.com>
2016-07-25 08:30:27 +0200

    in progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-$(document).ready(function(){
-	var question = $(".question").hide();
-    $("#next").click(function(){
-        $(".question").show("slide");
-    });
+$(".all .question").each(function(i) {
+    if (i != 0)
+        $(this).hide();
+});
+
+$("#next").click(function(){
+    if ($(".all .question:visible").next().length != 0)
+        $(".all .question:visible").next().show("slide").prev().hide("slide");
+    else {
+        $(".all .question:visible").hide("slide");
+        $(".all .question:first").show("slide");
+    }
+    return false;
+});
+
+$("#prev").click(function(){
+    if ($(".all .question:visible").prev().length != 0)
+        $(".all .question:visible").prev().show("slide").next().hide("slide");
+    else {
+        $(".all .question:visible").hide("slide");
+        $(".all .question:last").show("slide");
+    }
+    return false;
 });
