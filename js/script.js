7b267de0eac2f923402aa16a54f79420649e51ad
Redfish <lesonduko@gmail.com>
2016-07-26 16:10:25 +0200

    resolution en cours du quizz js affichage question reponse affichage bonne et mauvaise rep et defilement juska deux question

--- js/script.js
+++ js/script.js
@@ -1,30 +1,111 @@
 var question=[];
 var q1= "etes vous essez fort pour continuer ?";
 var rep=[];
-var rep1="oui";
-var rep2="non";
-var rep3="pietre";
-var rep4="je ne sais pas";
+var q1rep1="oui";
+var q1rep2="non";
+var q1rep3="pietre";
+var q1rep4="je ne sais pas";
 
 
-var q = question.push([q1]);
-AfficheRep();
-AfficherQuest();
 
-function AfficherQuest(){
+AfficheRep1();
+AfficherQuest1();
+
+
+function AfficherQuest1(){
 $("#question").text(q1);
 
+
 }
 
 
-function AfficheRep(){
-$("#rep1").text(rep1);
-$("#rep2").text(rep2);
-$("#rep3").text(rep3);
-$("#rep4").text(rep4);
+function AfficheRep1(){
+$("#rep1").text(q1rep1);
+$("#rep2").text(q1rep2);
+$("#rep3").text(q1rep3);
+$("#rep4").text(q1rep4);
 }
 
-function ChangeColor(){
+var q2= "quel est mon années de naissances ?";
+var rep=[];
+var q2rep1="1987";
+var q2rep2="1664";
+var q2rep3="1897";
+var q2rep4="je ne sais pas";
+
+function Question2(){
+AfficheRep2();
+AfficherQuest2();
+}
+
+function AfficherQuest2(){
+$("#question").text(q2);
+
+
+}
+
+
+function AfficheRep2(){
+$("#rep1").text(q2rep1);
+$("#rep2").text(q2rep2);
+$("#rep3").text(q2rep3);
+$("#rep4").text(q2rep4);
+}
+
+var q3= "Avez Vous mal au cheveux apres 10h de code ,";
+var rep=[];
+var q3rep1="defois";
+var q3rep2="non";
+var q3rep3="jamais";
+var q3rep4="oui";
+
+function Question3(){
+AfficheRep3();
+AfficherQuest3();
+}
+
+function AfficherQuest3(){
+$("#question").text(q2);
+
+
+}
+
+
+function AfficheRep3(){
+$("#rep1").text(q3rep1);
+$("#rep2").text(q3rep2);
+$("#rep3").text(q3rep3);
+$("#rep4").text(q3rep4);
+}
+
+
+
+	$(".false").click(function(){
+	$(this).css("background-color", 'red');
+
+	});
+
+	$(".true").click(function(){
+	$(this).css("background-color", 'green');	
+
+	});
+
+		
+
+$("#next").click(function(){
+	Question2();
 	
 	
-}
+});
+
+$("#prev").click(function(){
+	AfficheRep1()
+	AfficherQuest1();
+	
+
+});
+		
+
+
+/*error: file write error (No space left on device)
+fatal: unable to write sha1 file*/
