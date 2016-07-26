4300aa32bfe458151951596902981ba753074db4
Tyjow <platon.jonathan@gmail.com>
2016-07-26 15:55:47 +0200

    design

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
+var inc = 1;
+
 var slider = $('.bxslider').bxSlider({
-	pagerCustom: '#bx-pager'
+	pagerCustom: '#bx-pager',
+	speed: 250,
 });
 
 $('#slider-next').click(function(){
+  inc++;
+  if (inc > 10){
+  	inc = 1;
+  }
   slider.goToNextSlide();
+  $(".count").text("Page " + inc + "/10");
 });
 
 $('#slider-prev').click(function(){
+  inc--;
+  if (inc < 1){
+  	inc = 10;
+  }
   slider.goToPrevSlide();
+  $(".count").text("Page " + inc + "/10");
 });
 
 
@@ -15,9 +28,16 @@ $('#slider-prev').click(function(){
 //#ff4d4d = rouge
 
 $(".false").click(function () {
-    $(this).css({"background-color": "#ff4d4d", "color": "#000"});
+    $(this).css({"background-color": "#ff4d4d", "color": "#000", "font-weight": "400"});
 });
 
 $(".true").click(function () {
-    $(this).css({"background-color": "#66ff66", "color": "#000"});
+    $(this).css({"background-color": "#66ff66", "color": "#000", "font-weight": "400"}).append(" : Félicitation !");
+});
+
+$("#particle").particleground({
+	dotColor: '#f2f2f2',
+    lineColor: '#f2f2f2',
+    parallax: true,
+	parallaxMultiplier: 5,
 });
