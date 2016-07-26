a9050a4e2e8017283929e91431c6604465fbfbd3
Megaju <julien_malle@outlook.fr>
2016-07-26 20:47:43 +0200

    Projet fini et mieux codé ^^

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
-$(".ans-good").on({
-    click: function(){
-        $("td").removeClass("danger");
-        $(".ans-good").addClass("success");
-    } 
+// passer les questions
+var next = 1;
+
+function limitSkip() {
+    if (next == 11) {
+        next = 10;
+    } else if (next == 0) {
+        next = 1;
+    }
+}
+
+$("#next").click(function () {
+    $("#Q" + next + "").css({
+        "display": "none"
+    });
+    next++;
+    limitSkip();
+    $("#Q" + next + "").css({
+        "display": "block"
+    });
 });
 
-$(".ans-bad").on({
-    click: function(){
-        $("td").removeClass("danger");
-        $("td").removeClass("success");
-        $(this).addClass("danger");
-    }
-})
+$("#last").click(function () {
+    $("#Q" + next + "").css({
+        "display": "none"
+    });
+    next--;
+    limitSkip();
+    $("#Q" + next + "").css({
+        "display": "block"
+    });
+});
+
+$(".ans-good").click(function () {
+    $("#Q" + next + " td").css({
+        "background-color": "white",
+        "color": "black"
+    });
+    $(this).css({
+        "background-color": "green",
+        "color": "white"
+    });
+});
+
+$(".ans-bad").click(function () {
+    $("#Q" + next + " td").css({
+        "background-color": "white",
+        "color": "black"
+    });
+    $(this).css({
+        "background-color": "red",
+        "color": "white"
+    });
+});
