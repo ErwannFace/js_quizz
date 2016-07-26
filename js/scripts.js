7335534caec8bc7dbb1526e483338038ff2dabfa
nabil face <yampolos@gmail.com>
2016-07-26 16:45:32 +0200

    debut du code js

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+
+
+/*var questions = [
+        {
+            "question"    :   "Question 1: c'est qui qui quoi?",
+              
+            "reponses"    :  ["rien","quoi quoi","Albert Einstein!","hum"],
+            "bingo"       :   "Albert Einstein",
+        },
+
+        {
+            "question"      :   "Question 2: c'est quand qui quoi?",
+              
+            "reponses"       :   ["beh..","tu disais","je ne sais pas","hum"],
+            "correct"       :   "beh..",
+        },
+        ];
+
+var activeQuestion = 0;*/
+var question = "Question 1: c'est qui qui quoi?";
+var options = ["Rien","Quoi quoi","Albert Einstein!","Hum"];
+var bingo = "Albert Einstein";
+
+
+    $(".activeQuest").text(question);
+    $(".reponseOne").text(options[0]);
+    $(".reponseTwo").text(options[1]);
+    $(".reponseThree").text(options[2]);
+    $(".reponseFour").text(options[3]);
+
+    $(".reponseOne,.reponseTwo,.reponseThree,.reponseFour").click(function(){
+   //alert("hey");
+   
+   if(options.click() == bingo){
+    alert('yeh');
+   }
+   else{alert('heyhey');}
+ 
+        
+  
+
+    
+});
+
+
