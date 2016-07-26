a3a4b23281098d249d02454646214ccc08b0dc56
red <knoppix@Microknoppix.(none)>
2016-07-26 22:41:39 +0200

    bouton suivant et precedents changement des couleur et avancer sur les deux premiere question (blocage)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*variable 1*/
 var question=[];
 var q1= "etes vous essez fort pour continuer ?";
 var rep=[];
@@ -7,25 +8,25 @@ var q1rep3="pietre";
 var q1rep4="je ne sais pas";
 
 
-
+/*call display reponse1 and question1*/
 AfficheRep1();
 AfficherQuest1();
 
-
+/*display question1*/
 function AfficherQuest1(){
 $("#question").text(q1);
 
 
 }
 
-
+/*display reponse1*/
 function AfficheRep1(){
 $("#rep1").text(q1rep1);
 $("#rep2").text(q1rep2);
 $("#rep3").text(q1rep3);
 $("#rep4").text(q1rep4);
 }
-
+/*variable 2*/
 var q2= "quel est mon années de naissances ?";
 var rep=[];
 var q2rep1="1987";
@@ -33,25 +34,26 @@ var q2rep2="1664";
 var q2rep3="1897";
 var q2rep4="je ne sais pas";
 
+/*call display q2*/
 function Question2(){
 AfficheRep2();
 AfficherQuest2();
 }
-
+/*display q2*/
 function AfficherQuest2(){
 $("#question").text(q2);
 
 
 }
 
-
+/*display rep2*/
 function AfficheRep2(){
 $("#rep1").text(q2rep1);
 $("#rep2").text(q2rep2);
 $("#rep3").text(q2rep3);
 $("#rep4").text(q2rep4);
 }
-
+/*variable 3*/
 var q3= "Avez Vous mal au cheveux apres 10h de code ,";
 var rep=[];
 var q3rep1="defois";
@@ -59,18 +61,20 @@ var q3rep2="non";
 var q3rep3="jamais";
 var q3rep4="oui";
 
+
+/*call display3*/
 function Question3(){
 AfficheRep3();
 AfficherQuest3();
 }
-
+/*display question3*/
 function AfficherQuest3(){
 $("#question").text(q2);
 
 
 }
 
-
+/*display rep3*/
 function AfficheRep3(){
 $("#rep1").text(q3rep1);
 $("#rep2").text(q3rep2);
@@ -78,7 +82,7 @@ $("#rep3").text(q3rep3);
 $("#rep4").text(q3rep4);
 }
 
-
+/*change the color*/
 
 	$(".false").click(function(){
 	$(this).css("background-color", 'red');
@@ -91,13 +95,13 @@ $("#rep4").text(q3rep4);
 	});
 
 		
-
+/*button next*/
 $("#next").click(function(){
 	Question2();
-	
+	$(this).css("background-color", 'white')
 	
 });
-
+/*button previous*/
 $("#prev").click(function(){
 	AfficheRep1()
 	AfficherQuest1();
