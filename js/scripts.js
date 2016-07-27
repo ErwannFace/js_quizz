428e0ed4dc11fffe36bf55e03fbb661b8f33a4b7
nabil face <yampolos@gmail.com>
2016-07-27 12:14:13 +0200

    algo done pour une seule question youpiii

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,62 @@
 
 
-/*var questions = [
-        {
-            "question"    :   "Question 1: c'est qui qui quoi?",
-              
-            "reponses"    :  ["rien","quoi quoi","Albert Einstein!","hum"],
-            "bingo"       :   "Albert Einstein",
-        },
-
-        {
-            "question"      :   "Question 2: c'est quand qui quoi?",
-              
-            "reponses"       :   ["beh..","tu disais","je ne sais pas","hum"],
-            "correct"       :   "beh..",
-        },
-        ];
-
-var activeQuestion = 0;*/
 var question = "1: c'est qui qui quoi?";
 var options = ["Rien","Quoi quoi","Albert Einstein!","Hum"];
-var bingo = "Albert Einstein";
+var bingo = options[2];
+var score = 0;
 
 
-    $(".activeQuest").text(question);
+ $(".activeQuest").text(question);
     $(".reponseOne").text(options[0]);
     $(".reponseTwo").text(options[1]);
     $(".reponseThree").text(options[2]);
     $(".reponseFour").text(options[3]);
+    $(".monScore").text(score);
 
-    $(".reponseOne,.reponseTwo,.reponseThree,.reponseFour").click(function(){
-   //alert("hey");
-   
-   if(options.click() == bingo){
-    alert('yeh');
-   }
-   else{alert('heyhey');}
- 
-      //https://www.youtube.com/watch?v=d_UuOVhuCF8  
-  
 
-    
+
+$(".reponseOne,.reponseTwo,.reponseThree,.reponseFour").click( function () {
+    var choix= $(this).html();
+    if (choix == bingo) {
+        $(this).css('background-color','green');
+        $(this).css('color','white');
+        $(".monScore").text(score++);
+    } else {
+        $(this).css('background-color','red');
+        $(this).css('color','white');
+        $(".monScore").text(score);
+    }
 });
 
+   
 
+
+
+
+
+//mes premiers tests !!!!!
+
+//$(".reponseOne,.reponseTwo,.reponseThree,.reponseFour").click(function(){
+//alert("hey");
+   
+/*
+$(".reponseThree").click(function(){  
+    $(".reponseThree").css('background-color','green');
+    $(".reponseThree").css('color','white');
+});
+
+$(".reponseOne").click(function(){  
+    $(".reponseOne").css('background-color','red');
+    $(".reponseOne").css('color','white');
+});
+
+$(".reponseTwo").click(function(){  
+    $(".reponseTwo").css('background-color','red');
+    $(".reponseTwo").css('color','white');
+});
+
+$(".reponseFour").click(function(){  
+    $(".reponseFour").css('background-color','red');
+    $(".reponseFour").css('color','white');
+});
+*/
