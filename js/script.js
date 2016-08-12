bb6dfd7100b995ce024b8118ffe7321ea71630f3
Mopi <solene.huault@gmail.com>
2016-08-12 19:23:01 +0200

    pretty score + display progress + alert when quizz is completed

--- js/script.js
+++ js/script.js
@@ -2,64 +2,80 @@ var index = 0;
 var answers = [];
 answers.length = all_qst.length;
 var score = 0;
+var progress = 1;
+var current_question;
+var right_answer_index;
+var right_answer;
+var user_answer;
 
 /* /Display functions\ */
 display_qst(all_qst[index]);
 
 function display_qst(question) {
+	current_question = all_qst[index];
+	right_answer_index = current_question[5]; //returns integer
+	right_answer = current_question[right_answer_index];
+	user_answer = answers[index];
 	$("h2").html(question[0]);
 	$("tr:first td:first").html(question[1]);
 	$("tr:first td:last").html(question[2]);
 	$("tr:last td:first").html(question[3]);
 	$("tr:last td:last").html(question[4]);
 	$("td").css("background-color", "transparent");
+	$("p:first").html(index+1 + "/10");
+	$("p:last").html("Score: " + score);
 	
 	check_answer();
+	
+	//Disable the 'previous' button if the first question is displayed
+	if (index == 0) {
+		$("button:first").attr("disabled", true);
+	}
+	else {
+		$("button:first").attr("disabled", false);	
+	}
+	//Disable the 'next' button if the last question is displayed
+	if (index == all_qst.length-1) {
+		$("button:last").attr("disabled", true);
+	}
+	else {
+		$("button:last").attr("disabled", false);	
+	}
 }
 
 $("button:first").click(function previous_qst() {
-	if (index != 0) {
-		index--;
-		display_qst(all_qst[index]);
-	}
+	index--;
+	display_qst(all_qst[index]);
 });
 
 $("button:last").click(function next_qst() {
-	if (index < all_qst.length - 1) {
-		index++;
-		display_qst(all_qst[index]);
-	}
+	index++;
+	display_qst(all_qst[index]);
 });
 /* \Display functions/ */
-
 
 $("td").click(function () {
 	store_answer(this);
 	check_answer();
-	if (answers_full()) {
-		display_score();
-	}
+	all_answered();
 });
 
 //store the user answer in a array
 function store_answer(target) {
 	if (!answers[index]) {
 		answers[index] = $(target).html();
-		console.log(answers);
+		//count the score and display it
+		user_answer = answers[index];
 		set_score();
+		display_qst(all_qst[index]);
 	}
 }
 
 function check_answer() {
-	var current_question = all_qst[index];
-	var right_answer_index = current_question[5]; //returns integer
-	var right_answer = current_question[right_answer_index];
-	var user_answer = answers[index];
 	//Search the user answer in the table,
 	for (var i = 0 ; i < $("td").length ; i++) {
 		var x = $("td")[i];
 		if ($(x).html() == user_answer) {
-			//Compare the user answer with the right answer,
 			//Color the background green is is answer is correct...
 			if (user_answer == right_answer) {
 				$(x).css("background-color", "#5CB85C");	
@@ -72,28 +88,19 @@ function check_answer() {
 	}
 }
 
-function set_background() {
-}
-
-function answers_full() {
+//Alert the user when all the questions have been answered
+function all_answered() {
 	for (var i = 0 ; i < answers.length ; i++) {
 		if (answers[i] == undefined) {
 			return false;
 		}
 	}
-	return true;
+	alert("Nice, you completed the quizz =)!");
 }
 
+//Increase the score when a correct answer is given
 function set_score() {
-	var current_question = all_qst[index];
-	var right_answer_index = current_question[5]; //returns integer
-	var right_answer = current_question[right_answer_index];
-	var user_answer = answers[index];
-	if (right_answer == user_answer) {
+		if (right_answer == user_answer) {
 		score++;
 	}
-}
-
-function display_score() {
-	alert(score + "/10");
 }
