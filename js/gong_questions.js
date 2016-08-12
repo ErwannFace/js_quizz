20f955160207e34e5354d0edc8dee934288d562d
Mopi <solene.huault@gmail.com>
2016-08-12 19:41:42 +0200

    other quizz

---
+++ js/gong_questions.js
@@ -0,0 +1,57 @@
+var question1 = ["En quelle année sera découverte la planète Gong ?",
+								 "2032",
+								 "2042",
+								 "2045",
+								 "2062",
+								 1];
+
+var question2 = ["Que signifie l’acronyme C.O.I.T. ?",
+								 "Collège Officiel des Ingénieurs en Télécommunications",
+								 "Convention Organisée sur l’Industrie Tertiaire",
+								 "Compagnie d’Opéra Invisible du Tibet",
+								 "Collectif pour l’Objectivication des Insectes Tachetés",
+								 3];
+
+var question3 = ["Qu’offre Zero The Hero à manger au chat de Yoni ?",
+								 "une pizza Margherita",
+								 "du fish and chips",
+								 "un Big Kahuna Burger",
+								 "des champignons mexicains",
+								 2];
+
+var question4 = ["Quel est l’intrus dans cette liste ?",
+								 "Bloomdido Bad De Grass",
+								 "Dingo Virgin",
+								 "Francis Bacon",
+								 "Steve Hillage",
+								 4];
+
+var question5 = ["Dans quel véhicule circulent les extra-terrestes ?",
+								 "des soucoupes volantes",
+								 "des saucières volantes",
+								 "des théières volantes",
+								 "des soupières volantes",
+								 3];
+
+var question6 = ["Quelle est la profession de Sélène ?",
+								 "prostituée",
+								 "professeure",
+								 "prêtresse",
+								 "princesse",
+								 1];
+
+var question9 = ["De quelle planète est originaire Zero The Hero ?",
+								 "Gong",
+								 "Kobaïa",
+								 "la Terre",
+								 "Mars",
+								 3];
+
+var question10 = ["Mais, au fait, quelle est la thématique de ce quizz ?",
+									"Gong, le meilleur groupe de rock progressif de tous les temps !",
+									"ne cliquez pas ici",
+									"ceci est une mauvaise réponse",
+									"non, franchement, la bonne réponse est la première",
+									1];
+
+var all_qst = [question1,	question2, question3, question4, question5,	question6, question9,	question10];
